f3944bc0d1aed2e6ffd48c4b6a10e6ca74d05886
jackgao118 <gao.hui1@husky.neu.edu>
2020-03-29 01:50:16 -0400

    update angular url

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -4,15 +4,15 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/huigao1/courses')
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/huigao1/courses`)
       .then(response => response.json());
   findCourseById = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/huigao1/courses/${courseId}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/huigao1/courses/${courseId}`)
       .then((response => response.json()));
   findLessonForModule = (moduleId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/huigao1/modules/${moduleId}/lessons`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/huigao1/modules/${moduleId}/lessons`)
       .then(response => response.json());
   findModuleForCourse = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/huigao1/courses/${courseId}/modules`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/huigao1/courses/${courseId}/modules`)
       .then(response => response.json());
 }

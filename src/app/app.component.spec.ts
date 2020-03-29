2f92492544c763463b38aa2a92992cd182d895ab
jackgao118 <gao.hui1@husky.neu.edu>
2020-03-29 01:03:23 -0400

    client-angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wbdv-sp2o-hgao-client-angular'`, () => {
+  it(`should have as title 'wbdv-sp20-huigao-client-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wbdv-sp2o-hgao-client-angular');
+    expect(app.title).toEqual('wbdv-sp20-huigao-client-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('wbdv-sp2o-hgao-client-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('wbdv-sp20-huigao-client-angular app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { HomeComponent } from './home/home.component';
+import {CourseServiceClient} from './services/CourseServiceClient';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { ModuleListComponent } from './module-list/module-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseViewerComponent,
+    HomeComponent,
+    LessonTabsComponent,
+    ModuleListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
